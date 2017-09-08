af8344cc8cc85aa80e19250369a8f574eadc3771
Madoka Fujimaki Ahlsèn <madoka@bazooka.se>
2017-09-08 11:09:00 +0200

    Add project files.

---
+++ MadokaDiagnostisktProv/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MadokaDiagnostisktProv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
